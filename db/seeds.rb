5e5273884c99b9d79c0f5b99b8b90039c3b881c9
kaileeagray <kaileeagray@gmail.com>
2016-07-13 14:58:36 -0500

    added db seeds with Faker

---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# # Create users
+# 10.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   user = User.create(
+#     username: first_name + "-" + last_name,
+#     email: Faker::Internet.safe_email(first_name + "." + last_name),
+#     password: Faker::Internet.password
+#   )
+# end
+#
+#
+# # Create choices
+# 200.times do
+#   choice = Choice.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: Faker::Number.between(1, 11))
+# end
+
+User.all.each do |user|
+  choice = Choice.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: user.id)
+end
+
+# Create pros/cons
+Choice.all.each do |choice|
+  pro = Pro.create(choice_id: choice.id, user_id: Faker::Number.between(1, 11), description: Faker::ChuckNorris.fact, rank: Faker::Number.between(1, 100))
+  con = Con.create(choice_id: choice.id, user_id: Faker::Number.between(1, 11), description: Faker::StarWars.quote, rank: Faker::Number.between(1, 100))
+  pro.save!
+  con.save!
+end
+
+#
+# 50.times do
+#   pro = Pro.create(choice_id: Faker::Number.between(1, 200), user_id: Faker::Number.between(1, 11), description: Faker::ChuckNorris.fact, rank: Faker::Number.between(1, 100))
+#   con = Con.create(choice_id: Faker::Number.between(1, 200), user_id: Faker::Number.between(1, 11), description: Faker::StarWars.quote, rank: Faker::Number.between(1, 100))
+#   pro.save!
+#   con.save!
+# end
