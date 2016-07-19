8c060543820862035fc0d18ea64a2cdecb2cf84e
kaileeagray <kaileeagray@gmail.com>
2016-07-19 13:34:02 -0500

    add spec.md file and note progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,48 @@
-# Create users
-10.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  user = User.create(
-    username: first_name + "-" + last_name,
-    email: Faker::Internet.safe_email(first_name + "." + last_name),
-    password: Faker::Internet.password
-  )
-end
-
-User.create(
-  username: "test",
-  email: "test@sample.com",
-  password: "test"
-)
-
-# Create lists
-100.times do
-  list = List.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: Faker::Number.between(1, 11))
-end
+# # Create users
+# 10.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   user = User.create(
+#     username: first_name + "-" + last_name,
+#     email: Faker::Internet.safe_email(first_name + "." + last_name),
+#     password: Faker::Internet.password
+#   )
+# end
+#
+# User.create(
+#   username: "test",
+#   email: "test@sample.com",
+#   password: "test"
+# )
+#
+# # Create lists
+# 100.times do
+#   list = List.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: Faker::Number.between(1, 11))
+# end
+#
+# User.all.each do |user|
+#   5.times do
+#     list = List.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: user.id)
+#   end
+# end
+#
+# # Create pros/cons
+# List.all.each do |list|
+#   5.times do
+#     pro = Pro.create(list_id: list.id, user_id: Faker::Number.between(1, 11), description: Faker::ChuckNorris.fact, rank: Faker::Number.between(1, 10))
+#     con = Con.create(list_id: list.id, user_id: Faker::Number.between(1, 11), description: Faker::StarWars.quote, rank: Faker::Number.between(1, 10))
+#     pro.save!
+#     con.save!
+#   end
+# end
+#
+#
+# 50.times do
+#   pro = Pro.create(list_id: Faker::Number.between(1, 150), user_id: Faker::Number.between(1, 11), description: Faker::ChuckNorris.fact, rank: Faker::Number.between(1, 10))
+#   con = Con.create(list_id: Faker::Number.between(1, 150), user_id: Faker::Number.between(1, 11), description: Faker::StarWars.quote, rank: Faker::Number.between(1, 10))
+#   pro.save!
+#   con.save!
+# end
 
 User.all.each do |user|
   5.times do
@@ -34,12 +58,4 @@ List.all.each do |list|
     pro.save!
     con.save!
   end
-end
-
-
-50.times do
-  pro = Pro.create(list_id: Faker::Number.between(1, 150), user_id: Faker::Number.between(1, 11), description: Faker::ChuckNorris.fact, rank: Faker::Number.between(1, 10))
-  con = Con.create(list_id: Faker::Number.between(1, 150), user_id: Faker::Number.between(1, 11), description: Faker::StarWars.quote, rank: Faker::Number.between(1, 10))
-  pro.save!
-  con.save!
 end
---
+++ spec.md
@@ -0,0 +1,18 @@
+# Specifications for the Sinatra Assessment
+
+Specs:
+- [x] Use Sinatra to build the app
+- [x] Use ActiveRecord for storing information in a database
+- [x] Include more than one model class (list of model class names: Con, List, Pro, User)
+- [x] Include at least one has_many relationship (User has_many Lists, List has_many Pros, List has_many Cons, User has_many Pros through Lists, User has_many Cons through Lists)
+- [x] Include user accounts
+- [x] Ensure that users can't modify content created by other users (users are allowed to suggest pro/cons to other users but not edit title, description, or delete)
+- [ ] Include user input validations -- need to add create, edit list/pro/con validations. validations complete for log in and account creation
+- [ ] Display validation failures to user with error message -- errors with login, account creation. add to create, edit list/pro/con
+- [ ] Your README.md includes a short description, install instructions, a contributors guide and a link to the license for your code
+
+Confirm
+- [x] You have a large number of small Git commits
+- [x] Your commit messages are meaningful
+- [x] You made the changes in a commit that relate to the commit message
+- [x] You don't include changes in a commit that aren't related to the commit message
