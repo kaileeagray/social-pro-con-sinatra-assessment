8c5fe2a3eeef6f9b97c833f7857bdd1b242e7516
kaileeagray <kaileeagray@gmail.com>
2016-07-20 10:48:46 -0500

    fix duplicate creation if title/description left unchanged in edit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,94 +24,88 @@ class ListsController < ApplicationController
     end
   end
 
-    post '/lists/new' do
-      if params[:title] == ""
-        @error = "Title of list cannot be left blank."
-        erb :'/lists/create_list'
-      end
-      if logged_in? && params[:title] != ""
-        @list = List.new(title: params[:title])
-        @list.description = params[:description] if params[:description] != ""
-        @list.user_id = current_user.id
-        @list.save
-        erb :'/procons/create_procons'
-      else
-        redirect '/lists/new'
-      end
+  post '/lists/new' do
+    if params[:title] == ""
+      @error = "Title of list cannot be left blank."
+      erb :'/lists/create_list'
     end
+    if logged_in? && params[:title] != ""
+      @list = List.new(title: params[:title])
+      @list.description = params[:description] if params[:description] != ""
+      @list.user_id = current_user.id
+      @list.save
+      erb :'/procons/create_procons'
+    else
+      redirect '/lists/new'
+    end
+  end
 
-    get '/lists/:id/edit' do
-      @list = List.find(params[:id].to_i)
+  get '/lists/:id/edit' do
+    @list = List.find(params[:id])
+    if !logged_in?
+      redirect "/login"
+    elsif current_user.lists.include?(@list) && logged_in?
       erb :'/lists/edit_list'
-      # if current_user.lists.include?(@list) && logged_in?
-      #
-      # else
-      #   erb :'/lists/show_list'
-      # end
-      # if !logged_in?
-      #   redirect "/login" # redirect if not logged in
-      # else
-      #   if list = current_user.lists.find_by(params[:id])
-      #     erb :'/lists/edit_list'
-      #   else
-      #     redirect '/lists'
-      #   end
-      # end
+    else
+      erb :'/lists/show_list'
+    end
+  end
+
+  get '/lists/:id' do
+    @list = List.find(params[:id].to_i)
+    if current_user.lists.include?(@list) && logged_in?
+      erb :'/lists/user_show_list'
+    else
+      erb :'/lists/show_list'
+    end
+    # if logged_in?
+    #   @list = list.find(params[:id].to_i)
+    #   erb :'/lists/show_list'
+    # else
+    #   redirect '/login'
+    # end
+  end
+
+
+
+
+
+  post '/lists/:id/edit' do
+    @list = List.find(params[:id])
+    if !logged_in?
+      redirect "/login" # redirect if not logged in
+    elsif params[:title] == " " || params[:description] == " "
+      erb :'/lists/edit_list'
+    elsif params[:title] == @list.title && params[:description] == @list.description
+      redirect "/lists/#{@list.id}"
+    else
+      current_user.lists.find_by(params[:id]).update(title: params[:title], description: params[:description])
+      redirect "/lists/#{@list.id}"
+    end
+  end
+
+  delete '/lists/:id/delete' do #delete action
+    @errors = []
+    list = List.find(params[:id])
+    if logged_in? && list == current_user.lists.find(params[:id])
+      list.delete
+      redirect "/lists"
+    else
+      @errors << "Sorry, your delete action cannot be completed because you were not logged in. Please try again." if !logged_in?
+      @errors << "Sorry, your delete action cannot be completed. You may only delete your own lists. Please try again." if list != current_user.lists.find(params[:id])
+      erb :"/users/lists/#{params[:id]}"
     end
 
-    get '/lists/:id' do
-      @list = List.find(params[:id].to_i)
-      if current_user.lists.include?(@list) && logged_in?
-        erb :'/lists/user_show_list'
-      else
-        erb :'/lists/show_list'
-      end
-      # if logged_in?
-      #   @list = list.find(params[:id].to_i)
-      #   erb :'/lists/show_list'
-      # else
-      #   redirect '/login'
-      # end
+  end
+
+  get '/lists/:id/procons/new' do
+    @list = List.find(params[:id].to_i)
+    if current_user.lists.include?(@list) && logged_in?
+      erb :'/procons/edit_procons'
+    else
+      erb :'/procons/suggest_procons'
     end
-
-
-
-
-
-    post '/lists/:id/edit' do
-      @list = List.find(params[:id])
-      if !logged_in?
-        redirect "/login" # redirect if not logged in
-      elsif params[:title] == " " || params[:description] == " "
-        erb :'/lists/edit_list'
-      else
-        current_user.lists.find_by(params[:id]).update(title: params[:title], description: params[:description])
-        redirect "/lists/#{@list.id}"
-      end
-    end
-
-    delete '/lists/:id/delete' do #delete action
-      @errors = []
-      list = List.find(params[:id])
-      if logged_in? && list == current_user.lists.find(params[:id])
-        list.delete
-        redirect "/lists"
-      else
-        @errors << "Sorry, your delete action cannot be completed because you were not logged in. Please try again." if !logged_in?
-        @errors << "Sorry, your delete action cannot be completed. You may only delete your own lists. Please try again." if list != current_user.lists.find(params[:id])
-        erb :"/users/lists/#{params[:id]}"
-      end
-
-    end
-
-    get '/lists/:id/procons/new' do
-      @list = List.find(params[:id].to_i)
-      if current_user.lists.include?(@list) && logged_in?
-        erb :'/procons/edit_procons'
-      else
-        erb :'/procons/suggest_procons'
-      end
-    end
+  end
 
 
 end
