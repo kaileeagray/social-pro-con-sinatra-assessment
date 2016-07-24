f4dc5c616d63ed510d915bc42ebd75c92a73e52f
kaileeagray <kaileeagray@gmail.com>
2016-07-24 13:36:32 -0500

    add scrapes from the onion

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.create(
 
 # Create lists
 100.times do
-  list = List.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: Faker::Number.between(1, 11))
+  list = List.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence, user_id: Faker::Number.between(1, 11), created_at: Faker::Time.between(9.days.ago, Date.today, :midnight))
 end
 
 User.all.each do |user|
@@ -39,7 +39,7 @@ end
 User.create(
   username: "test3",
   email: "test3@sample.com",
-  password: "test3"
+  password: "test3isthebest?"
 )
 
 # with the random select of user_ids most of the pros/cons were from other users. So, add this seed to make sure the user has the largest number of pros/cons in their lists
@@ -58,3 +58,60 @@ User.all.each do |user|
     end
   end
 end
+
+# use info from The Onion site
+
+User.create(
+  username: "the_onion",
+  email: "theonion@sample.com",
+  password: "theonionisfunny123!"
+)
+
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+
+class Scraper
+    ## I picked out a few of the pro/con lists and scraped them for their content.
+
+    @@all = ["http://www.theonion.com/graphic/pros-and-cons-of-screen-time-for-kids-38445", "http://www.theonion.com/graphic/pros-and-cons-of-going-to-grad-school-38467", "http://www.theonion.com/graphic/pros-and-cons-of-the-greek-system-38515", "http://www.theonion.com/graphic/pros-and-cons-raising-minimum-wage-50476", "http://www.theonion.com/graphic/pros-and-cons-freelance-employment-51603", "http://www.theonion.com/graphic/pros-and-cons-political-correctness-52003", "http://www.theonion.com/graphic/pros-and-cons-helicopter-parenting-52130", "http://www.theonion.com/graphic/pros-and-cons-attending-college-52701", "http://www.theonion.com/graphic/pros-and-cons-profit-colleges-53065", "http://www.theonion.com/graphic/pros-and-cons-taking-gap-year-52852", "http://www.theonion.com/graphic/pros-and-cons-standardized-testing-50388", "http://www.theonion.com/graphic/the-pros-and-cons-of-open-plan-offices-38377", "http://www.theonion.com/graphic/the-pros-and-cons-of-paying-college-athletes-38321"]
+
+
+    def visit_all
+      @@all.each do |url|
+        @url = url
+        self.get_page(url)
+        self.make_list
+      end
+    end
+
+
+    def get_page(url)
+      # The .get_page instance method will be responsible
+      # for using Nokogiri and open-uri to grab the entire HTML document from the web page.
+      @doc = Nokogiri::HTML(open(url))
+    end
+
+
+    def make_list
+      list = List.new
+      list.user_id = User.find_by(username: "the_onion").id
+      list.title =  @doc.css(".content-header").text.strip
+      list.description = "Source: #{@url}\n\n" + @doc.css(".content-text b").first.text
+      list.save
+      @doc.css(".content-text ul").first.css("li").collect do |pro|
+        pro = Pro.create(list_id: list.id, user_id: User.find_by(username: "the_onion").id, description: pro.text, weight: Faker::Number.between(0, 10))
+        pro.save!
+      end
+      @doc.css(".content-text ul")[1].css("li").collect do |con|
+        con = Con.create(list_id: list.id, user_id: User.find_by(username: "the_onion").id, description: con.text, weight: Faker::Number.between(0, 10))
+        con.save!
+      end
+      list.save!
+    end
+
+end
+
+Scraper.new.visit_all
